0284aa8251a3aa466651b7d0b179107d6bec9069
Victor Lucas <victorlsilva@outlook.com.br>
2021-03-25 20:22:32 -0300

    Sexto Commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let ImageLogoWhite   = document.getElementById("Img-Logo-White-Branca");
 let ImageLogoBlue    = document.getElementById("Img-Logo-White-Azul");
 let PanelWindowsUser = document.getElementById("Panel-Windows-User");
+let Screen1          = document.getElementById("Screen-1");
+let Screen2          = document.getElementById("Screen-2");
 
-function EventLogo(){
+function EventLogo()
+{
     ImageLogoWhite.style.display = 'none';
     
-    if(ImageLogoBlue.style.display !== 'block'){
+    if(ImageLogoBlue.style.display !== 'block')
+    {
         ImageLogoBlue.style.display = 'block';
     }
 
@@ -13,7 +17,8 @@ function EventLogo(){
 
 function EventLogoExit(){
    
-    if(ImageLogoBlue.style.display  === 'block'){
+    if(ImageLogoBlue.style.display  === 'block')
+    {
         ImageLogoBlue.style.display  = 'none';
         ImageLogoWhite.style.display = 'block';
         ImageLogoWhite.style.left    = '17px';
@@ -21,11 +26,32 @@ function EventLogoExit(){
 
 }
 
-function ExibPanelWindows(){
-    if(PanelWindowsUser.style.display !== 'block'){
+function ExibPanelWindows()
+{
+    if(PanelWindowsUser.style.display !== 'block')
+    {
         PanelWindowsUser.style.display = 'block';
     }
-    else{
+    else
+    {
         PanelWindowsUser.style.display = 'none';
+    }
+}
+
+function myFunction()
+{
+    myVar = setInterval(SliderMicrosoft , 2000);
+}
+
+function SliderMicrosoft() {
+    Screen1.style.display = 'block';
+
+
+    if(Screen1.style.display === 'block')
+    {
+        Screen1.style.display = 'none';
+        Screen2.style.display = 'block';
+           Screen2.style.animation = "TrocaDeSlider2 1s"; 
+            
     }
 }
